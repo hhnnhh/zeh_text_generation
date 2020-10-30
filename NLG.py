8379ec36e7c3568571a2496193a010f0c2a5e817
Hannah <32333241+hhnnhh@users.noreply.github.com>
2020-10-30 13:15:05 +0100

    remove model.py

--- NLG.py
+++ NLG.py
@@ -1,7 +1,11 @@
+
+## Pretrained Model for English Text - XL-NET
 #from transformers import AutoModelWithLMHead, AutoTokenizer
 
 #model = AutoModelWithLMHead.from_pretrained("xlnet-base-cased", return_dict=True)
 #tokenizer = AutoTokenizer.from_pretrained("xlnet-base-cased")
+
+## Pretrained model
 
 from transformers import AutoModelWithLMHead, AutoTokenizer
 
@@ -10,9 +14,9 @@ model = AutoModelWithLMHead.from_pretrained("anonymous-german-nlp/german-gpt2")
 
 
 # Padding text helps XLNet with short prompts - proposed by Aman Rusia in https://github.com/rusiaaman/XLNet-gen#methodology
-PADDING_TEXT = data[1:500]
+PADDING_TEXT = clean_data[1:500]
 print(PADDING_TEXT)
-prompt = "Ada liebte es, wenn"
+prompt = "Alev hatte eine Katze und"
 inputs = tokenizer.encode(PADDING_TEXT + prompt, add_special_tokens=False, return_tensors="pt")
 
 max_length = len(PADDING_TEXT)
@@ -21,3 +25,8 @@ prompt_length = len(tokenizer.decode(inputs[0], skip_special_tokens=True, clean_
 outputs = model.generate(inputs, max_length=max_length, do_sample=True, top_p=0.95, top_k=60)
 generated = prompt + tokenizer.decode(outputs[0])[prompt_length:]
 generated
+
+
+# if not done before, strip newline now
+text = generated.replace('\n',' ')
+text
